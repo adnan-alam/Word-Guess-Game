61c1a2617dd4738a43a9240ffa010831a01ee693
Adnan Alam <adnan320324@gmail.com>
2017-10-05 13:04:30 +0600

    updated the methods and variables name

--- game/main.py
+++ game/main.py
@@ -1,47 +1,51 @@
+from random import choice
+import time
+
 
 class WordGuess:
     
-    def __init__(self,word_list_file):
-        self.word_list_file = word_list_file
+    def __init__(self,word_stock_file):
+		
+        self.word_stock_file = word_stock_file
         self.words = {}
-    
-	# takes user name as input and greets user
-    def greetings(self):
-        name = str(input("Enter your name : "))
+   
+    # takes user name as input and print greetings
+    def get_greetings(self):
+        
+        name = str(input("Enter your name : ").strip())
         welcome = "{}, welcome to Word Guess Game!".format(name)
         print(welcome)
+    
+    # return word list consist of words and parts of speech from the given txt file	
+    def get_word_list(self):
         
-    # loads self.words dictionary and returns words_list 
-    def loadWordList(self):
-        with open(self.word_list_file, "r") as file:
+        with open(self.word_stock_file, "r") as file:
             content = file.read().splitlines()
         
         for line in content:
-            word, part_speech = line.split("-")
-            self.words[word] = part_speech
+            word, parts_speech = line.split("-")
+            self.words[word] = parts_speech
     
         words_list = list(self.words.keys())
         return words_list
     
-    # randomly chooses word from words_list and returns it 
-    def wordToGuess(self):
-        from random import choice
+    # returns a randomly choosen word from word list
+    def get_word(self):
         
-        word_to_guess = choice(self.loadWordList())
+        word_to_guess = choice(self.get_word_list())
         return word_to_guess
     
-    # gives hints to user
-    def hintsForUser(self,word_to_guess):
-        import time
+    # gives hints to user about the word to be guessed	
+    def get_hints(self,word_to_guess):
         
         self.word_to_guess = word_to_guess 
         
         word_length = len(word_to_guess)
-        first_char = word_to_guess[:2]
-        last_char = word_to_guess[-2:]
-        part_speech = self.words[word_to_guess]
+        first_chars = word_to_guess[:2]
+        last_chars = word_to_guess[-2:]
+        parts_speech = self.words[word_to_guess]
         
-        hints = [[word_length, first_char, last_char, part_speech],
+        hints = [[word_length, first_chars, last_chars, parts_speech],
              ["Word's length","First two letters","Last two letters","Parts of speech"]]
     
         total_hints = len(hints[0])    
@@ -52,30 +56,27 @@ class WordGuess:
             print("{} --> {}".format(hints[1][i],hints[0][i]))
             print()
             time.sleep(2)
-	
         
-    # takes user input and checks if it matches with word_to_guess
-
+     
+    # checks user input with the word to be guessed and prints result
     @property   
-    def userInput(self):
-        word_to_guess = self.wordToGuess()
-    
-        self.greetings()
-        self.hintsForUser(word_to_guess)
+    def start_game(self):
         
-        user_input = str(input("Guess the word : "))
-        if user_input == word_to_guess:
+        word_to_guess = self.get_word()
+    
+        self.get_greetings()
+        self.get_hints(word_to_guess)
+        
+        user_input = str(input("Guess the word : ").strip())
+        if user_input.lower() == word_to_guess.lower():
             print("Yes, your guess is right. \nThe word was, {}.".format(word_to_guess))
         else:
             print("Sorry, your guess is wrong. \nThe word was, {}.".format(word_to_guess))
         
     
         
-
-# initializes the game with given file 
-wordGuessGame = WordGuess("word-list.txt")
-
-# starts the game
-wordGuessGame.userInput
-        
     
+word_guess_game = WordGuess("word-list.txt")
+
+
+word_guess_game.start_game
